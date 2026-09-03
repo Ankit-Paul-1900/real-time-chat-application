8e1a399b7f3d74d324ada95fb036bffa0d3866ad
Ankit-Paul-1900 <msclabs.work@gmail.com>
2026-09-03 22:24:26 +0530

    proxy updated temp. 2nd time

--- wt-clone/proxy.ts
+++ wt-clone/proxy.ts
@@ -2,13 +2,11 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
-
     console.log("PATH:", request.nextUrl.pathname);
     console.log("COOKIES:", request.cookies.getAll());
     console.log("TOKEN:", request.cookies.get("Token")?.value);
 
     const path = request.nextUrl.pathname;
-
     const publicPath = ["/login"];
 
     const token = request.cookies.get("Token")?.value || "";
@@ -25,3 +23,7 @@ export function proxy(request: NextRequest) {
 
     return NextResponse.next();
 }
+
+export const config = {
+    matcher: ["/login", "/", "/profile"],
+};
