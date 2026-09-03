08f3b69d09f3f438282b1e73e74fad0447cee5f3
Ankit-Paul-1900 <msclabs.work@gmail.com>
2026-09-03 22:12:28 +0530

    proxy updated temp.

--- wt-clone/proxy.ts
+++ wt-clone/proxy.ts
@@ -1,18 +1,27 @@
-import {NextResponse} from "next/server";
-import {NextRequest} from "next/server";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-export function proxy(request:NextRequest){
-    const path=request.nextUrl.pathname;
-    const publicPath=["/login"];
-    const token= request.cookies.get("Token")?.value ||"";
-     if (publicPath.includes(path)){
-        return NextResponse.next()
-     }
-    if (!publicPath.includes(path) && !token){
-        return NextResponse.redirect(new URL("/login",request.nextUrl))
+export function proxy(request: NextRequest) {
+
+    console.log("PATH:", request.nextUrl.pathname);
+    console.log("COOKIES:", request.cookies.getAll());
+    console.log("TOKEN:", request.cookies.get("Token")?.value);
+
+    const path = request.nextUrl.pathname;
+
+    const publicPath = ["/login"];
+
+    const token = request.cookies.get("Token")?.value || "";
+
+    if (publicPath.includes(path)) {
+        return NextResponse.next();
     }
-    
-}
-export const config = {
-  matcher: ['/login','/','/profile'],
+
+    if (!token) {
+        return NextResponse.redirect(
+            new URL("/login", request.url)
+        );
+    }
+
+    return NextResponse.next();
 }
