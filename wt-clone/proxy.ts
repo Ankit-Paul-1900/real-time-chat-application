df0c2f0c60d2017ccc34e1267a41495b1c5f77a9
Ankit-Paul-1900 <msclabs.work@gmail.com>
2026-09-03 22:53:38 +0530

    proxy updated commented out

--- wt-clone/proxy.ts
+++ wt-clone/proxy.ts
@@ -1,29 +1,29 @@
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import { NextRequest } from "next/server";
 
-export function proxy(request: NextRequest) {
-    console.log("PATH:", request.nextUrl.pathname);
-    console.log("COOKIES:", request.cookies.getAll());
-    console.log("TOKEN:", request.cookies.get("Token")?.value);
+// export function proxy(request: NextRequest) {
+//     console.log("PATH:", request.nextUrl.pathname);
+//     console.log("COOKIES:", request.cookies.getAll());
+//     console.log("TOKEN:", request.cookies.get("Token")?.value);
 
-    const path = request.nextUrl.pathname;
-    const publicPath = ["/login"];
+//     const path = request.nextUrl.pathname;
+//     const publicPath = ["/login"];
 
-    const token = request.cookies.get("Token")?.value || "";
+//     const token = request.cookies.get("Token")?.value || "";
 
-    if (publicPath.includes(path)) {
-        return NextResponse.next();
-    }
+//     if (publicPath.includes(path)) {
+//         return NextResponse.next();
+//     }
 
-    if (!token) {
-        return NextResponse.redirect(
-            new URL("/login", request.url)
-        );
-    }
+//     if (!token) {
+//         return NextResponse.redirect(
+//             new URL("/login", request.url)
+//         );
+//     }
 
-    return NextResponse.next();
-}
+//     return NextResponse.next();
+// }
 
-export const config = {
-    matcher: ["/login", "/", "/profile"],
-};
+// export const config = {
+//     matcher: ["/login", "/", "/profile"],
+// };
